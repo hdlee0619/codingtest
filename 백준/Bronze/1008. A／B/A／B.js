57b43b9a571b8a574e6eede1d9576a7823bac86e
이현동 (Hyeondong Lee) <96946263+hdlee0619@users.noreply.github.com>
2023-01-20 13:00:00 +0900

    [Bronze V] Title: A/B, Time: 140 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin').toString().split('\n'); // '/dev/stdin'
+
+let numbers = [];
+
+for (let i = 0; i < input.length; i++) {
+    if (input[i] !== '') {
+        numbers.push(input[i].split(' '));
+    }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    let num1 = Number(numbers[i][0]);
+    let num2 = Number(numbers[i][1]);
+
+    console.log(num1 / num2);
+}

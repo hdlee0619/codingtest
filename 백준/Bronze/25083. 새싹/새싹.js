9274206f6c893a821a5b1fef4ec51d53af4559cd
이현동 (Hyeondong Lee) <96946263+hdlee0619@users.noreply.github.com>
2023-01-21 22:00:48 +0900

    [Bronze V] Title: 새싹, Time: 108 ms, Memory: 9272 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,9 @@
+const inputData = [`         ,r'"7`, "r`-_   ,'  ,/", ` \\. ". L_r'`, '   `~\\/', '      |', '      |'];
+
+let solution = (inputData) => {
+    for (let i = 0; i < inputData.length; i++) {
+        console.log(inputData[i]);
+    }
+};
+
+solution(inputData);

2c2d2b85a197b4344eeab7a75419dbe0684fe50b
이현동 (Hyeondong Lee) <96946263+hdlee0619@users.noreply.github.com>
2023-01-21 21:54:48 +0900

    [Bronze V] Title: 고양이, Time: 108 ms, Memory: 9280 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.js
@@ -0,0 +1,9 @@
+const inputData = ['\\    /\\', " )  ( ')", '(  /  )', ' \\(__)|'];
+
+let solution = (inputData) => {
+    for (let i = 0; i < inputData.length; i++) {
+        console.log(inputData[i]);
+    }
+};
+
+solution(inputData);

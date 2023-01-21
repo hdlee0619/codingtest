69a3b52d0cf2b3cfe4e2cbd24f320351ada121f7
이현동 (Hyeondong Lee) <96946263+hdlee0619@users.noreply.github.com>
2023-01-21 21:57:16 +0900

    [Bronze V] Title: 개, Time: 120 ms, Memory: 9284 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.js
@@ -0,0 +1,9 @@
+const inputData = ['|\\_/|', '|q p|   /}', '( 0 )"""\\', '|"^"`    |', '||_/=\\\\__|'];
+
+let solution = (inputData) => {
+    for (let i = 0; i < inputData.length; i++) {
+        console.log(inputData[i]);
+    }
+};
+
+solution(inputData);

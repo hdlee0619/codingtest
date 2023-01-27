6282a8e60d5f84ec76481e683af010bb86a347b4
이현동 (Hyeondong Lee) <96946263+hdlee0619@users.noreply.github.com>
2023-01-27 17:47:31 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,5 @@
 function solution(seoul) {
     const data = seoul.indexOf('Kim');
-    let string = ['김서방은', String(data) + '에', '있다'];
-    return string.join(" ");
+    let string = '김서방은 '+ String(data) +'에 '+'있다';
+    return string;
 }

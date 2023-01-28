35f12e91d03b39ff60c6a6bf79bcfc8b1347b21d
이현동 (Hyeondong Lee) <96946263+hdlee0619@users.noreply.github.com>
2023-01-28 16:34:49 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 49.06 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,15 @@
+const solution = (arr) => {
+    let j = 1;
+    let tempArray = [];
+    let array = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) {
+            tempArray.push(arr[i]);
+        }
+    }
+
+    if (tempArray[tempArray.length - 2] === tempArray[tempArray.length - 1]) {
+        tempArray.pop();
+    }
+    return tempArray;
+};

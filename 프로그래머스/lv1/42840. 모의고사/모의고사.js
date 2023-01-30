01419f6d0c020bcb82326d8ba52e7ec89acd8d4d
hdlee0619 <dlgusehd1211@naver.com>
2023-01-30 16:11:17 +0900

    Fix: 모의고사 수정

--- 프로그래머스/lv1/42840. 모의고사/모의고사.js
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -1,4 +1,32 @@
 function solution(answers) {
-    var answer = [];
+    const supoja = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+    ];
+    const supojaLength = [supoja[0].length, supoja[1].length, supoja[2].length];
+
+    const num = [0, 0, 0];
+    const answer = [];
+
+    for (let i = 0; i < answers.length; i++) {
+        for (let j = 0; j < supojaLength.length; j++) {
+            if (answers[i] === supoja[j][i % supojaLength[j]]) num[j]++;
+            // answers[0] === supoja[0][0]
+            // answers[0] === supoja[0][1]
+            // answers[0] === supoja[0][2]
+            // 위와 같이 비교하여 서로 같은 값일떄 배열에 count를 + 해준다.
+        }
+    }
+
+    const maxNum = Math.max(...num); // num배열의 최고값을 구한다.
+
+    num.forEach((a, i) => {
+        if (a === maxNum) {
+            //a와 maxNum의 값을 비교하여 같으면 answer배열에 +1씩 해준다.
+            answer.push(i + 1);
+        }
+    });
+
     return answer;
 }

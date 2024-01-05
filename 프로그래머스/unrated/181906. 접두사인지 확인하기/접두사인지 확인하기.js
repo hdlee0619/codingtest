e2000e654ad762d1d710ada65a8210a59ae6d30d
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-01-05 17:33:21 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.17 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,8 +1,4 @@
 function solution(my_string, is_prefix) {
-    let prefix = [];
-    for (let i = 0; i < my_string.length; i++) {
-        prefix.push(my_string.slice(0, i + 1));
-    }
-    
-    return prefix.includes(is_prefix) ? 1 : 0;
+    var answer = 0;
+    return answer;
 }
